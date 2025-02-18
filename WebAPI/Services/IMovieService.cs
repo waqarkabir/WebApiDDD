2f39aacbbffacaf22d9a63d52813db9596dfa734
waqarkabir <waqarkabir10@gmail.com>
2025-02-18 10:57:51 +0500

    Adding IMovieService Interface

---
+++ WebAPI/Services/IMovieService.cs
@@ -0,0 +1,13 @@
+﻿using WebAPI.DTOs;
+
+namespace WebAPI.Services
+{
+    public interface IMovieService
+    {
+        Task<MovieDto> CreateMovieAsync(CreateMovieDto command);
+        Task<MovieDto?> GetMovieByIdAsync(Guid id);
+        Task<IEnumerable<MovieDto>> GetAllMoviesAsync();
+        Task UpdateMovieAsync(Guid id, UpdateMovieDto command);
+        Task DeleteMovieAsync(Guid id);
+    }
+}
