a6aedda86c0b0ca619f2399a083e76e9777cc9cb
waqarkabir <waqarkabir10@gmail.com>
2025-02-18 09:31:14 +0500

    Adding DBContext

---
+++ WebAPI/Persistence/MovieDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPI.Models;
+
+namespace WebAPI.Persistence
+{
+    public class MovieDbContext(DbContextOptions<MovieDbContext> options): DbContext(options)
+    {
+        public DbSet<Movie> Movies => Set<Movie>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("app");
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(MovieDbContext).Assembly);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
