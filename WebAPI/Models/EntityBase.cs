3b129f04fc0f46033b90665ce86180088fe045c8
waqarkabir <waqarkabir10@gmail.com>
2025-02-18 08:35:33 +0500

    Add project files.

---
+++ WebAPI/Models/EntityBase.cs
@@ -0,0 +1,14 @@
+﻿namespace WebAPI.Models
+{
+    public abstract class EntityBase
+    {
+        public Guid Id { get; private set; } = Guid.NewGuid();
+        public DateTimeOffset Created { get; private set; } = DateTimeOffset.UtcNow;
+        public DateTimeOffset LastModified { get; private set; } = DateTimeOffset.UtcNow;
+
+        protected EntityBase()
+        {
+            LastModified = DateTimeOffset.UtcNow;
+        }
+    }
+}
