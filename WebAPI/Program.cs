f86fb19adfa51646e6a849b518bf1a9bc1096201
waqarkabir <waqarkabir10@gmail.com>
2025-02-18 09:54:33 +0500

    Registering the DbContex

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,8 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddDbContext<MovieDbContext>(options =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    options.UseNpgsql(connectionString);
+});
 
 var app = builder.Build();
 
@@ -17,5 +25,5 @@ app.UseHttpsRedirection();
 app.MapGet("/", () => "Hello World!")
    .Produces(200, typeof(string));
 
-app.Run();
+await app.RunAsync();
 
