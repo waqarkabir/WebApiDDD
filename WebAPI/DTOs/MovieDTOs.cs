93d5e376ae049c64543c1e27a9e09c97a9ea41ed
waqarkabir <waqarkabir10@gmail.com>
2025-02-18 10:48:07 +0500

    Adding the DTOs - ViewModels

---
+++ WebAPI/DTOs/MovieDTOs.cs
@@ -0,0 +1,8 @@
+﻿namespace WebAPI.DTOs
+{
+    public record CreateMovieDto(string Title, string Genre, DateTimeOffset ReleaseDate, double Rating);
+
+    public record UpdateMovieDto(string Title, string Genre, DateTimeOffset ReleaseDate, double Rating);
+
+    public record MovieDto(Guid Id, string Title, string Genre, DateTimeOffset ReleaseDate, double Rating);
+}
